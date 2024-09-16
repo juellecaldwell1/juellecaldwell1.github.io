5161c5a61b9c760c161e8d00bfc42c206da56239
juellecaldwell1 <juelle_caldwell@insideranken.org>
2024-09-16 15:35:59 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,7 @@ function App() {
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Navbar />
         
-      <Routes>
-        <Route path="/" element={<div>
-          <Header />
-          <Banner />
-          <About />
-          <Services />
-          <Work />
-          <Contact />
-        </div>}>
-        </Route>
-        <Route path="/resume" element={<Resume />} />
-
-      </Routes>
+      
 
       {/* <div className='h-[4000px]'></div>  */}
     </div>
